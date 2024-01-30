def36b6780f6dd4093c7cdf1c7f79d2056cb344c
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2024-01-30 22:03:21 +0100

    mint post as NFT, launchpad, swap on testnet

---
+++ nuxt.config.ts
@@ -0,0 +1,164 @@
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config 
+export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        {
+          "name": "viewport",
+          "content": "width=device-width, initial-scale=1"
+        },
+        {
+          "charset": "utf-8"
+        }
+      ],
+      link: [
+        { // Bootstrap
+          rel: "stylesheet",
+          href: "	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+        },
+        { // Bootstrap icons
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+        },
+        { // Custom
+          rel: "stylesheet",
+          href: "/css/custom.css"
+        }
+      ],
+      script: [
+        {
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        }
+      ]
+    }
+  },
+  components: false,
+  css: [
+    'vue-toastification/dist/index.css' 
+  ],
+  modules: [
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+  ],
+  router: {
+    options: {
+      hashMode: false
+    }
+  },
+  runtimeConfig: {
+    public: {
+      activityPointsAddress: "",
+      airdropApAddress: "", // chat token claim for APs
+      airdropClaimDomainsAddress: "", // chat token claim for domain holders
+      blockExplorerBaseUrl: "",
+      chatChannels: { // go to Orbis Dashboard (https://useorbis.com/dashboard), create a new Project and then create a new Context for each of the channels below
+        "general": "kjzl6cwe1jw14anfen1dc5341ku30h2vq0bm593yqhrrnefn96diezzm71d09fc", // general discussion channel
+        "memesImages": "kjzl6cwe1jw14baa970et2v1t5dow17o99i86l7a731pvbz8n1ncakq02e1j80z",
+        "shill": "kjzl6cwe1jw149xdx2rhelufxdl02mh0mdhpwl4kp0fnb5srr8qdrpbd5nzvoed",
+        "nftLaunchpad": "kjzl6cwe1jw145u6xa5xq8lrdhgwbcfk9ik79lonmkk9inn36e444sw43ykel3u",
+        "friendKeys": "kjzl6cwe1jw14b20jz0shleevpvoa1ba5m09ggc7jf46o26d686iqcnedcazoqc",
+      },
+      chatTokenAddress: "", // chat token address
+      chatTokenImage: "", // chat token image
+      chatTokenSymbol: "MCHAT", // chat token symbol or name
+      domainRequiredToPost: true,
+      expiryCollections: 1000 * 60 * 60 * 24 * 7, // must be in milliseconds (0 means no expiration)
+      expiryUsernames: 1000 * 60 * 60 * 24 * 7, // must be in milliseconds (0 means no expiration)
+      favicon: "/img/favicon.svg",
+      fileUploadEnabled: true, // enable/disable file uploads (enable only if external file storage is used, e.g. IPFS via Spheron)
+      fileUploadSizeLimit: 1 * 1024 * 1024, // max file upload size in bytes (1 * 1024 * 1024 = 1 MB)
+      fileUploadTokenService: process.env.FILE_UPLOAD_SERVICE || "netlify", // "netlify" or "vercel" (or leave empty for no file uploads)
+      getPostsLimit: 30, // number of posts to fetch from Orbis in the getPosts() function
+      governanceUrl: "", // governance url (snapshot, Tally, etc.)
+      iggyPostAddress: "0x99Dbf11aCd46baFBCE82506FaeB4F13E6Ea1726A",
+      iggyPostMinterAddress: "0xabf9960132818049340253C3Ca0551F92Db856d7",
+      iggyPostStatsAddress: "0x0BF6333Fc85159663A30Ac89FD02c5031B97c5ee",
+      imagekitEndpoint: process.env.IMAGEKIT_ENDPOINT,
+      imagekitPublicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+      ipfsGateway: "https://cloudflare-ipfs.com/ipfs/",
+      keysAddress: "", // FriendKeys contract address 
+      keysFeatured: ["tempe", "tekr"],
+      linkPreviews: process.env.LINK_PREVIEW_SERVICE || "netlify", // "netlify", "vercel", or "microlink" (or leave empty for no link previews)
+      lpTokenAddress: "", // liquidity pool token (token to stake in the staking contract)
+      lpTokenSymbol: "LP tokens", // LP token symbol
+      marketplacePostNftUrl: "https://testnets.opensea.io/assets/mumbai/0x99Dbf11aCd46baFBCE82506FaeB4F13E6Ea1726A",
+      marketplacePostNftItemUrl: "https://testnets.opensea.io/assets/mumbai/0x99Dbf11aCd46baFBCE82506FaeB4F13E6Ea1726A/", // url (append nft id to it)
+      marketplaceNftCollectionBaseUrl: "https://testnets.opensea.io/assets/mumbai/", // url (append nft address to it)
+      newsletterLink: "https://paragraph.xyz/@iggy?modal=subscribe",
+      nftDefaultRatio: 1, // default ratio for the NFT price bonding curve
+      nftLaunchpadBondingAddress: "0xBdaba8106cdC29420c9A7Bb31066ED79c9b6Be74", // NFT launchpad with bonding curve contract address
+      nftLaunchpadLatestItems: 4, // number of latest NFTs to show in the NFT launchpad
+      orbisTest: false, // if true, test context will be used instead of the production one
+      orbisTestContext: "kjzl6cwe1jw145tfqv2eqv8tiz6puo27meyz4smz40atppuc13tulqca87k35z2", // test context
+      previewImage: "/img/covers/cover.svg",
+      previewImageAirdrop: "/img/covers/cover-airdrop.svg",
+      previewImageNftCollection: "/img/covers/cover-nft-collection.svg",
+      previewImageNftCreate: "/img/covers/cover-nft-create.svg",
+      previewImageNftLaunchpad: "/img/covers/cover-nft-launchpad.svg",
+      previewImagePost: "/img/covers/cover-post.svg",
+      previewImagePostNft: "/img/covers/cover-post-nft.svg",
+      previewImageProfile: "/img/covers/cover-profile.svg",
+      previewImageStake: "/img/covers/cover-stake.svg",
+      profileMintedPostIdsMax: 36, // max number of minted post ids to show in the profile page
+      projectMetadataTitle: "ModeChat | Web3 Social on Mode Network",
+      projectName: "ModeChat",
+      projectDescription: "ModeChat is the first decentralized social network on Mode L2 chain.",
+      projectTwitter: "https://twitter.com/iggysocial",
+      projectUrl: "https://modechat.xyz", // without trailing slash!
+      punkMinterAddress: "0x63f8691b048e68E1C3d6E135aDc81291A9bb1987", // punk domain minter contract address
+      punkNumberOfPrices: 5, // number of different prices (based on domain length), usually 1 (price()) or 5 (price1char() - price5char())
+      punkTldAddress: "0x32749Ab66ef1B85aFfF3425d5766d8025E407769", // punk domain TLD address
+      randomPostsNumber: 1, // number of random post NFTs to show in the sidebar widget
+      rpcCustom: process.env.RPC_CUSTOM || "https://sepolia.mode.network/", // Custom RPC URL
+      showFeatures: { // show/hide features in sidebars (if you have too many "true", make the sidebar scrollable --> sidebarLeftSticky: false)
+        "activityPoints": false, 
+        "airdrop": false, 
+        "friendKeys": false, 
+        "governance": false,
+        "newsletter": false, 
+        "nftLaunchpad": true, 
+        "randomMintedPosts": true, 
+        "swap": true, 
+        "stake": false, 
+        "sendTokens": true, 
+        "spotify": false
+      }, 
+      showRepliesOnHomepage: true, // show replies on the homepage  
+      sidebarLeftSticky: true, // make the left sidebar sticky (always visible)
+      spotifyPlaylistId: "5y7f2Wxfq49G5KuNQfMPbk", // enter just the ID of the playlist (not the full URL)  
+      stakingContractAddress: "", // this is also the stake/gov token address
+      stakeTokenSymbol: "MCG", // stake token symbol (governance token symbol)
+      supportedChainId: 919,
+      swapPriceImpactMaxBps: 1000, // max price impact in bips (1 bps = 0.01%, 1000bps = 10%) for the swap function
+      swapRouterAddress: "0x4A82158ff4B0504F3DB4c7555FfB6298452985E2", // iggy swap router contract address
+      tenorApiKey: process.env.TENOR_KEY || "",
+      tldName: ".testmode",
+      tokenAddress: null, // leave null if it's a native token of the chain
+      tokenDecimals: 18,
+      tokenSymbol: "ETH"
+    }
+  },
+  vite: {
+    build: {
+      target: ['es2020'] // fix big integer literals error
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: 'globalThis'  // fix nuxt3 global
+        },
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            process: true,  // fix nuxt3 process
+            buffer: true
+          }),
+          NodeModulesPolyfillPlugin()
+        ],
+        target: "es2020" // fix big integer literals error
+      }
+    }
+  }
+})
