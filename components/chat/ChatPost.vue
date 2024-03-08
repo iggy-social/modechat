b5f0c44c59466fdaaca3da0550ad3fe0113c755c
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2024-03-08 13:09:26 +0100

    friend key link in posts and on profile page

--- components/chat/ChatPost.vue
+++ components/chat/ChatPost.vue
@@ -17,6 +17,7 @@
       <!-- post author and timestamp -->
       <p class="card-subtitle mb-2 text-muted">
         <NuxtLink class="link-without-color hover-color" :to="'/profile/?id='+String(showDomainOrFullAddress)">{{showDomainOrAddressOrAnon}}</NuxtLink>
+        <span v-if="$config.showFeatures.friendKeys && getModechatName"> · <NuxtLink class="link-without-color hover-color" :to="'/keys/?username='+getModechatName"><i class="bi bi-key-fill"></i></NuxtLink></span>
         <span v-if="post.timestamp"> · <NuxtLink class="link-without-color hover-color" :to="'/post/?id='+post.stream_id">{{timeSince}}</NuxtLink></span>
       </p>
 
@@ -273,6 +274,16 @@ export default {
     getMintedPostTokenId() {
       if (this.customDataType === "mintedPost") {
         return String(this.post.content.data.nftTokenId);
+      }
+
+      return null;
+    },
+
+    getModechatName() {
+      if (this.authorDomain) {
+        if (this.authorDomain.endsWith(this.$config.tldName)) {
+          return this.authorDomain.replace(this.$config.tldName, "");
+        }
       }
 
       return null;
